2655f25d0be62e5d886ce776b3b60e4a0db92040
k.nomo <k.nomoto1225@gmail.com>
2021-03-23 07:16:59 +0000

    0323test

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -2,6 +2,6 @@ class Notice < ApplicationRecord
   belongs_to :user
   belongs_to :mygroup
   
-  validates :title, presence: true, length: {maximum: 25 }
+  validates :title, presence: true, length: {maximum: 30 }
   validates :content, presence: true, length: {maximum: 100 }
 end
