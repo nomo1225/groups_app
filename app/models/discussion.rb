62578f85ed8b8f7bd22e18dec2911c6e7800d50b
kohei.nomoto <k.nomoto1225@gmail.com>
2021-04-16 08:26:33 +0000

    discussion has_many opinions

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,7 @@
 class Discussion < ApplicationRecord
   belongs_to :user
   belongs_to :mygroup
+  has_many :opinions, dependent: :destroy
   
   validates :title, presence: true, length: {maximum: 30 }
   validates :content, presence: true, length: {maximum: 150 }
