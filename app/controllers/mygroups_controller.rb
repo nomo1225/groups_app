24785102da54433857df51c2616f50556cb69d26
kohei.nomoto <k.nomoto1225@gmail.com>
2021-05-25 01:02:35 +0000

    052502

--- app/controllers/mygroups_controller.rb
+++ app/controllers/mygroups_controller.rb
@@ -76,8 +76,8 @@ class MygroupsController < ApplicationController
   
   def members #メンバー一覧
     @user = User.find(current_user.id)
-    @relationships = @mygroup.relationships.order(created_at: :asc).page(params[:page]).per(15) #参加順に表示
-    @members = @mygroup.members
+    # @relationships = @mygroup.relationships.order(created_at: :asc).page(params[:page]).per(15) #参加順に表示
+    @members = @mygroup.members.order(created_at: :asc).page(params[:page]).per(15) #ユーザー登録順
   end
   
   private
